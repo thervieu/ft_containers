659447b7a26510ce71b818537c410ecc425a9f8f
thervieu <thibault99hervieu@gmail.com>
2021-01-22 15:42:57 +0100

    not much tests doable yet as i want to a first version and there is a lot of dependencies of different functions (assign with clear and push_back ...)

--- incs/list.hpp
+++ incs/list.hpp
@@ -3,6 +3,8 @@
 
 # include "listIterator.hpp"
 
+# include <limits>
+
 namespace ft
 {
 	template < class T, class Alloc = allocator<T> >
@@ -14,16 +16,16 @@ namespace ft
 
 			typedef Alloc allocator_type;
 
+			typedef T value_type;
 			typedef T& reference;
 			typedef T& const_reference;
 			typedef T* pointer;
 			typedef T* const_pointer;
-			typedef T value_type;
 
 			typedef listIterator<value_type> iterator;
-			typedef listIterator<value_type> const_iterator;
-			typedef listIterator<value_type> reverse_iterator;
-			typedef listIterator<value_type> const_reverse_iterator;
+			typedef constListIterator<value_type> const_iterator;
+			typedef reversListIterator<value_type> reverse_iterator;
+			typedef constReverseListIterator<value_type> const_reverse_iterator;
 
 			typedef size_t size_type;
 
@@ -37,72 +39,367 @@ namespace ft
 			allocator_type _alloc;
 
 			size_type		_length;
-		
+
+			Node<value_type>	*newNode(value_type value, node _prev, node _next)
+			{
+				Node<value_type> _new = new Node<value_type>;
+				_new->data = value;
+				_new->prev = _prev;
+				_new->next = _next;
+			}
+			void	initList(void)
+			{
+				_root = newNode(value_type(void), NULL, NULL);
+				_pastTheEnd = newNode(value_type(void), _root, NULL);
+				_root->next = _pastTheEnd; 
+			}
 		public:
 
-			list(void)
-			{};
-
-			list(const list &rhs)
+			explicit list (const allocator_type& alloc = allocator_type(void)): _root(0), _pastTheEnd(0), _alloc(alloc), _length(0)
 			{
-				*this = rhs;
-			};
+				initList();
+			}
+
+			explicit list (size_type n, const value_type& val = value_type(void),
+            	const allocator_type& alloc = allocator_type(void)): _root(0), _pastTheEnd(0), _alloc(alloc), _length(0)
+			{
+				initList();
+				assign(n, value);
+			}
+
+			template <class InputIterator>
+			list (InputIterator first, InputIterator last,
+        	const allocator_type& alloc = allocator_type(void)): _root(0), _pastTheEnd(0), _alloc(alloc), _length(0)
+			{
+				initList();
+				assign(first, last);
+			}
+
+			list (const list& x): _root(0), _pastTheEnd(0), _alloc(alloc), _length(0)
+			{
+				initList();
+				*this = other;
+			}
 
 			~list(void)
-			{};
-
-			list	&operator=(const list &rhs)
 			{
-				_ptr = rhs._ptr;
-				return (*this);
-			};
 
-			value_type		&operator&(void)
-			{
-				return (_ptr->data);
-			};
+			}
 
-			value_type		*operator*(void)
+			list&	operator= (const list& x)
 			{
-				return (_ptr->data);
-			};
+				
+			}
 
-			list	&operator++(void)
+			iterator	begin(void)
 			{
-				_ptr = _ptr->next;
-				return (*this);
-			};
+				return (iterator(_root->next));
+			}
 
-			list	&operator++(int)
+			const_iterator	begin(void) const
 			{
-				list rtn(*this);
-				operator++();
-				return (*this);
-			};
+				return (const_iterator(_root->next));
+			}
 
-			list	&operator--(void)
+			iterator	end(void)
 			{
-				_ptr = _ptr->next;
-				return (*this);
-			};
+				return (iterator(_pastTheEnd));
+			}
 
-			list	&operator--(int)
+			const_iterator	end(void) const
 			{
-				list rtn(*this);
-				operator--();
-				return (*this);
-			};
+				return (const_iterator(_pastTheEnd));
+			}
 
-			bool	operator==(const list &rhs) const
+			reverse_iterator	rbegin(void)
 			{
-				return (_ptr == rhs._ptr);
-			};
+				return (reverse_iterator(_root->next));
+			}
 
-			bool	operator!=(const list &rhs) const
+			const_reverse_iterator	rbegin(void) const
 			{
-				return (_ptr != rhs._ptr);
-			};
+				return (const_reverse_iterator(_root->next));
+			}
+
+			reverse_iterator	rend(void)
+			{
+				return (reverse_iterator(_pastTheEnd->prev));
+			}
+
+			const_reverse_iterator	rend(void) const
+			{
+				return (const_reverse_iterator(_pastTheEnd->prev));
+			}
+
+			bool empty(void) const
+			{
+				return (_length == 0);
+			}
+
+			size_type size(void) const
+			{
+				return (_length);
+			}
+
+			size_type max_size(void) const
+			{
+				return (std::numeric_limits<size_type>::max() / sizoef(Node<T>));
+			}
+
+			reference front(void)
+			{
+				return (_root->next->data);
+			}
+
+			const_reference front(void) const
+			{
+				return (_root->next->data);
+			}
+
+			reference back(void)
+			{
+				return (_pastTheEnd->prev->data);
+			}
+
+			const_reference back(void) const
+			{
+				return (_pastTheEnd->prev->data);
+			}
+
+			template <class InputIterator>
+			void assign (InputIterator first, InputIterator last)
+			{
+				clear();
+				while (first != last)
+				{
+					push_bask(*first);
+					first++;
+				}
+			}
+
+			void assign (size_type n, const value_type& val)
+			{
+				clear();
+				while (n--)
+					push_back(val);
+			}
+
+			// root	<-> beg		&& root <-  newBeg  -> beg
+			// root	 -> beg		&& root <-  newBeg <-> beg
+			// root	 -  beg 	&& root <-> newBeg <-> beg
+
+			void push_front (const value_type& val)
+			{
+				node newBeg = newNode(val, _root, _root->next);
+
+				_root->next->prev = newBeg;
+				_root->next = newBeg;
+				_length++;
+			}
+
+			// root <-> beg <-> newBeg && root  -  newBeg
+			// root <-  beg <-> newBeg && root  -> newBeg
+			// root <-  beg  -> newBeg && root <-> newBeg
+
+			void pop_front(void)
+			{
+				node delete_me = _root->next;
+
+				_root->next = _root->next->next;
+				_root->next->prev = _root;
+
+				delete delete_me;
+			}
+
+			// end	<-> _pte	&& end <-  newEnd  -> pte
+			// end	<- 	_pte	&& end <-> newEnd  -> pte
+			// end	 - 	_pte 	&& end <-> newEnd <-> pte
+
+			void push_back (const value_type& val)
+			{
+				node newEnd = newNode(val, _pastTheEnd->prev, _pastTheEnd);
+
+				_pastTheEnd->prev->next = newEnd;
+				_pastTheEnd->prev = newEnd;
+				_length++;
+			}
+
+			// newEnd <-> end <-> pte	&& newEnd  -  pte
+			// newEnd <-> end -> pte	&& newEnd <-  pte
+			// newEnd <- end -> pte		&& newEnd <-> pte
+
+			void pop_front(void)
+			{
+				node delete_me = _pastTheEnd->prev;
+
+				_pastTheEnd->prev = _pastTheEnd->prev->prev;
+				_pastTheEnd->prev->next = _pastTheEnd;
+
+				delete delete_me;
+			}
+
+			iterator insert (iterator position, const value_type& val)
+			{
+
+			}
+
+			void insert (iterator position, size_type n, const value_type& val)
+			{
+
+			}
+
+			template <class InputIterator>
+			void insert (iterator position, InputIterator first, InputIterator last)
+			{
+
+			}
+
+			iterator erase (iterator position)
+			{
+
+			}
+
+			iterator erase (iterator first, iterator last)
+			{
+
+			}
+
+			void swap (list& x)
+			{
+
+			}
+
+			void resize (size_type n, value_type val = value_type(void))
+			{
+
+			}
+
+			void clear(void)
+			{
+				node delete_me = _root->next;
+
+				while (delete_me != _pastTheEnd)
+				{
+					node _next = delete_me->next;
+					delete delete_me;
+					delete_me = _next;
+				}
+				
+				_root->next = _pastTheEnd;
+				_pastTheEnd->prev = _root;
+				_length = 0;
+				
+				return ;
+			}
+
+			void splice (iterator position, list& x)
+			{
+
+			}
+
+			void splice (iterator position, list& x, iterator i)
+			{
+
+			}
+
+			void splice (iterator position, list& x, iterator first, iterator last)
+			{
+
+			}
+
+			void remove (const value_type& val)
+			{
+
+			}
+
+			template <class Predicate>
+			void remove_if (Predicate pred)
+			{
+
+			}
+
+			void unique(void)
+			{
+
+			}
+
+			template <class BinaryPredicate>
+			void unique (BinaryPredicate binary_pred)
+			{
+
+			}
+
+			void merge (list& x)
+			{
+
+			}
+
+			template <class Compare>
+			void merge (list& x, Compare comp)
+			{
+
+			}
+
+			void sort(void)
+			{
+
+			}
+
+			template <class Compare>
+			void sort (Compare comp)
+			{
+
+			}
+
+			void reverse(void)
+			{
+
+			}
+
 	};
+
+	template <class T, class Alloc>
+	bool operator== (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs)
+	{
+
+	}
+
+	template <class T, class Alloc>
+	bool operator!= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs)
+	{
+
+	}
+
+	template <class T, class Alloc>
+	bool operator<  (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs)
+	{
+
+	}
+
+	template <class T, class Alloc>
+	bool operator<= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs)
+	{
+
+	}
+
+	template <class T, class Alloc>
+	bool operator>  (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs)
+	{
+
+	}
+
+	template <class T, class Alloc>
+	bool operator>= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs)
+	{
+
+	}
+
+	template <class T, class Alloc>
+	void swap (list<T,Alloc>& x, list<T,Alloc>& y)
+	{
+
+	}
 }
+
 
 #endif
--- incs/listIterator.hpp
+++ incs/listIterator.hpp
@@ -69,7 +69,7 @@ namespace ft
 
 			listIterator	&operator--(void)
 			{
-				_ptr = _ptr->next;
+				_ptr = _ptr->prev;
 				return (*this);
 			};
 
@@ -86,6 +86,237 @@ namespace ft
 			};
 
 			bool	operator!=(const listIterator &rhs) const
+			{
+				return (_ptr != rhs._ptr);
+			};
+	};
+	template <class T>
+	class constListIterator
+	{
+		public:
+		
+			typedef T value_type;
+			typedef T& reference;
+			typedef Node<T>* pointer;
+		
+		protected:
+			pointer	_ptr;
+		
+		public:
+
+			constListIterator(void)
+			{};
+
+			constListIterator(const constListIterator &rhs)
+			{
+				*this = rhs;
+			};
+
+			~constListIterator(void)
+			{};
+
+			constListIterator	&operator=(const constListIterator &rhs)
+			{
+				_ptr = rhs._ptr;
+				return (*this);
+			};
+
+			value_type		&operator&(void)
+			{
+				return (_ptr->data);
+			};
+
+			value_type		*operator*(void)
+			{
+				return (_ptr->data);
+			};
+
+			constListIterator	&operator++(void)
+			{
+				_ptr = _ptr->next;
+				return (*this);
+			};
+
+			constListIterator	&operator++(int)
+			{
+				constListIterator rtn(*this);
+				operator++();
+				return (*this);
+			};
+
+			constListIterator	&operator--(void)
+			{
+				_ptr = _ptr->prev;
+				return (*this);
+			};
+
+			constListIterator	&operator--(int)
+			{
+				constListIterator rtn(*this);
+				operator--();
+				return (*this);
+			};
+
+			bool	operator==(const constListIterator &rhs) const
+			{
+				return (_ptr == rhs._ptr);
+			};
+
+			bool	operator!=(const constListIterator &rhs) const
+			{
+				return (_ptr != rhs._ptr);
+			};
+	};
+	template <class T>
+	class reverseListIterator
+	{
+		public:
+		
+			typedef T value_type;
+			typedef T& reference;
+			typedef Node<T>* pointer;
+		
+		protected:
+			pointer	_ptr;
+		
+		public:
+
+			reverseListIterator(void)
+			{};
+
+			reverseListIterator(const reverseListIterator &rhs)
+			{
+				*this = rhs;
+			};
+
+			~reverseListIterator(void)
+			{};
+
+			reverseListIterator	&operator=(const reverseListIterator &rhs)
+			{
+				_ptr = rhs._ptr;
+				return (*this);
+			};
+
+			value_type		&operator&(void)
+			{
+				return (_ptr->data);
+			};
+
+			value_type		*operator*(void)
+			{
+				return (_ptr->data);
+			};
+
+			reverseListIterator	&operator++(void)
+			{
+				_ptr = _ptr->prev;
+				return (*this);
+			};
+
+			reverseListIterator	&operator++(int)
+			{
+				reverseListIterator rtn(*this);
+				operator++();
+				return (*this);
+			};
+
+			reverseListIterator	&operator--(void)
+			{
+				_ptr = _ptr->next;
+				return (*this);
+			};
+
+			reverseListIterator	&operator--(int)
+			{
+				reverseListIterator rtn(*this);
+				operator--();
+				return (*this);
+			};
+
+			bool	operator==(const reverseListIterator &rhs) const
+			{
+				return (_ptr == rhs._ptr);
+			};
+
+			bool	operator!=(const reverseListIterator &rhs) const
+			{
+				return (_ptr != rhs._ptr);
+			};
+	};
+	template <class T>
+	class constReverseListIterator
+	{
+		public:
+		
+			typedef T value_type;
+			typedef T& reference;
+			typedef Node<T>* pointer;
+		
+		protected:
+			pointer	_ptr;
+		
+		public:
+
+			constReverseListIterator(void)
+			{};
+
+			constReverseListIterator(const constReverseListIterator &rhs)
+			{
+				*this = rhs;
+			};
+
+			~constReverseListIterator(void)
+			{};
+
+			constReverseListIterator	&operator=(const constReverseListIterator &rhs)
+			{
+				_ptr = rhs._ptr;
+				return (*this);
+			};
+
+			value_type		&operator&(void)
+			{
+				return (_ptr->data);
+			};
+
+			value_type		*operator*(void)
+			{
+				return (_ptr->data);
+			};
+
+			constReverseListIterator	&operator++(void)
+			{
+				_ptr = _ptr->prev;
+				return (*this);
+			};
+
+			constReverseListIterator	&operator++(int)
+			{
+				constReverseListIterator rtn(*this);
+				operator++();
+				return (*this);
+			};
+
+			constReverseListIterator	&operator--(void)
+			{
+				_ptr = _ptr->next;
+				return (*this);
+			};
+
+			constReverseListIterator	&operator--(int)
+			{
+				constReverseListIterator rtn(*this);
+				operator--();
+				return (*this);
+			};
+
+			bool	operator==(const constReverseListIterator &rhs) const
+			{
+				return (_ptr == rhs._ptr);
+			};
+
+			bool	operator!=(const constReverseListIterator &rhs) const
 			{
 				return (_ptr != rhs._ptr);
 			};
