6600a20d1a16b25504957e6718da8a8a2ec83d33
thervieu <thibault99hervieu@gmail.com>
2021-08-10 16:06:00 +0200

    LGTM no leaks, ready to push v2

--- srcs/stack_tests.cpp
+++ srcs/stack_tests.cpp
@@ -17,24 +17,16 @@ static void top(void)
 	printTitle("Top");
 
 	ft::stack<int> ft1;
-	ft::stack<int> ft2;
 
 	std::stack<int> std1;
-	std::stack<int> std2;
 
 	ft1.push(0);
 	ft1.push(1);
 	ft1.push(2);
-	ft2.push(0);
-	ft2.push(1);
-	ft2.push(2);
 
 	std1.push(0);
 	std1.push(1);
 	std1.push(2);
-	std2.push(0);
-	std2.push(1);
-	std2.push(2);
 
 	std::cout << "ft1.size() == std1.size() : " << printBool((ft1.size() == std1.size())) << std::endl;
 	std::cout << "ft1.empty() == std1.empty() : " << printBool((ft1.empty() == std1.empty())) << std::endl;
